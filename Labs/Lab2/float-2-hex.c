11c2dbd79956bf48db24e84ec3f28c35b292150d
Omar Nassar <omariio1000@gmail.com>
2022-10-20 20:24:37 -0700

    started float 2 hex

--- Labs/Lab2/float-2-hex.c
+++ Labs/Lab2/float-2-hex.c
@@ -1,5 +1,16 @@
 /* Omar Nassar
  * October 18, 2022
  * Portland State University CS201
- * 
+ * Translate float to hex
  */
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    
+
+    return EXIT_SUCCESS;
+}
