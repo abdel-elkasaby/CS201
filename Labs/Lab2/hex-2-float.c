5ef85b2fb45c29317e30b247d74c8568a871f60b
Omar Nassar <omariio1000@gmail.com>
2022-10-24 23:09:29 -0700

    working on hex to float

--- Labs/Lab2/hex-2-float.c
+++ Labs/Lab2/hex-2-float.c
@@ -1,5 +1,17 @@
 /* Omar Nassar
  * October 18, 2022
  * Portland State University CS201
- * 
+ * Translate hex to float
  */
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]) {
+
+    return EXIT_SUCCESS;
+}
